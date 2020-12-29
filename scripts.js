0937890b83172485f28d3ce7b9621154af9625b1
David Sozashvili <datosoza.js@gmail.com>
2020-12-29 11:35:27 +0400

    +Css

--- scripts.js
+++ scripts.js
@@ -2,17 +2,18 @@
 const points = {2: 2, 3: 3, 4: 4, 5: 5, 6: 6, 7: 7, 8: 8, 9: 9, 10: 10, J: 10, Q: 10, K: 10, A: 11}
 
 
-function Player(status, money) {
+function Player(status, money, elemendId) {
     this.status = status;
     this.money = money;
     this.cards = [];
     this.points = 0;
+    this.cardSpace = document.getElementById(elemendId);
+
     this.take = function(deck){
-        this.cards.push(deck.getCard());
+        let lastCard = deck.getCard();
+        this.cards.push(lastCard);
         this.countPoints();
-        console.log('Player took cards');
-        console.log(this.cards);
-        console.log(this.points);
+        this.showCard(lastCard);
     }
     this.stop = function(){
         
@@ -20,7 +21,7 @@ function Player(status, money) {
     this.countPoints = function() {
         let aces = 0;
         this.points = 0;
-        this.cards.forEach(element => { // element = {number: 5, symbol: aguri}
+        this.cards.forEach(element => { 
             let point = points[element.number];
 
             if(element.number == 'A') {
@@ -34,11 +35,16 @@ function Player(status, money) {
             
         });
     }
+    this.showCard = function(card){
+        let img = document.createElement('img');
+        img.src = './images/png/' + card.number + card.symbol + '.png';
+        this.cardSpace.appendChild(img);
+    }
 }
 
 function Deck() {
     this.values = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A'];
-    this.symbols = ['aguri', 'guli', 'jvari', 'kvavi'];
+    this.symbols = ['D', 'H', 'S', 'C'];
     this.cards = [];
     this.build = function(){
         for(let i = 0; i < this.values.length; i++){
@@ -49,8 +55,7 @@ function Deck() {
                 this.cards.push(obj);
             }
         }
-        console.log('cards built');
-        console.log(this.cards);
+        
     }
 
     this.shuffle = function() {
@@ -62,8 +67,6 @@ function Deck() {
             this.cards.pop();
         }
         this.cards = shuffledCards;
-        console.log('cards shuffled');
-        console.log(this.cards);
     }
     this.getCard = function(){
         return this.cards.pop();
@@ -72,25 +75,94 @@ function Deck() {
 
 
 
-function checkWin(p1, p2) {
-    if(p1.points > 21) {
-        console.log('p2 won');
-        return;
-    }
-    if(p2.points > 21) {
-        console.log('p1 won');
-        return;
-    } 
-    if(p1.points >= p2.points) {
-        console.log('p1 won')
-    } else {
-        console.log('p2 won');
-    }
-}
+let player1 = new Player('Player', 1000, 'card-space1');
+let player2 = new Player('Dealer', 1000, 'card-space2');
 
-let dealer = new Player('Dealer', 1000000);
-let player = new Player('Player', 1000);
 let deck = new Deck();
 
 deck.build();
 deck.shuffle();
+
+setTimeout(() => {
+    player1.take(deck);
+    player2.take(deck);
+}, 1000);
+setTimeout(() => {
+    player1.take(deck);
+    player2.take(deck);
+}, 2000);
+
+const takeBtn1 = document.getElementById('btn1');
+const stopBtn1 = document.getElementById('stop1');
+
+const takeBtn2 = document.getElementById('btn2');
+const stopBtn2 = document.getElementById('stop2');
+
+stopBtn1.addEventListener('click', () => {
+    takeBtn1.style.display = 'none';
+    stopBtn1.style.display = 'none';
+    takeBtn2.style.display = 'inline-block';
+    stopBtn2.style.display = 'inline-block';
+});
+
+stopBtn2.addEventListener('click', () => {
+    takeBtn2.style.display = 'none';
+    stopBtn2.style.display = 'none';
+    endGame();
+})
+
+
+takeBtn1.addEventListener('click', () => {
+    player1.take(deck);
+    if(player1.points > 20) {
+        if(player1.points > 21) {
+            endGame();
+        } else {
+            takeBtn1.style.display = 'none';
+            stopBtn1.style.display = 'none';
+        }
+        
+    }
+});
+takeBtn2.addEventListener('click', () => {
+    player2.take(deck);
+    if(player2.points > 20) {
+        if(player2.points > 21) {
+            endGame();
+        } else {
+            takeBtn2.style.display = 'none';
+            stopBtn2.style.display = 'none';
+            takeBtn2.style.display = 'inline-block';
+            stopBtn2.style.display = 'inline-block';
+        }
+    }
+});
+
+function endGame() {
+    const center = document.querySelector('.center');
+    const p = center.children[0];
+    if(player1.points > 21) {
+        p.textContent = 'player2 won';
+    } else {
+        if(player2.points > 21) {
+            p.textContent = 'pkayer1 won';
+        } else {
+            if(player1.points >= player2.points) {
+                p.textContent = 'player1 won'
+            } else {
+                p.textContent = 'player2 won';
+            }
+        } 
+    }
+    
+    let a = document.createElement('a');
+    a.href = '';
+    a.textContent = 'new game';
+    center.appendChild(a);
+
+    takeBtn1.style.display = 'none';
+    takeBtn2.style.display = 'none';
+    stopBtn1.style.display = 'none';
+    stopBtn2.style.display = 'none';
+}
+
